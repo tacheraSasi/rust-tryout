184fc84b9ecf030a0472f01f091f29e5dcc1ada7
tacheraSasi <tacherasasi@gmail.com>
2024-10-24 01:25:00 +0300

    simple print smt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-- Guess a number --");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Something went, Failed to read the line");
+
+    
 }
+
